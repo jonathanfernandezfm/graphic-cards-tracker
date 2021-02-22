46424dbb31c4db431d26a44ba27ace884a514c89
Jonathan <jonathanfernandezfm@gmail.com>
2021-02-22 16:06:23 +0100

    💻 Better tracking and loggin into console

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const pccomponentes_json = require('./index/pccomponentes.json');
+const colors = require('colors');
 const { coolmod } = require('./services/coolmod');
 const { versus } = require('./services/versusgamers');
-const { pccomponentes } = require('./services/pccomponentes')
+const { pccomponentes } = require('./services/pccomponentes');
 const config = require('./config/config.json');
 
-
 setInterval(() => {
-  console.log("\x1b[36mCHECKING PCCOMPONENTES\x1b[0m");
-  if(pccomponentes_json.tracking) 
-    if(pccomponentes_json.tracking.length == 0)
-      console.log("Missing index. Generate it with 'npm run indexs'");
-  
-  pccomponentes_json.tracking.forEach(async ({id, name, link}, index) => {
-    pccomponentes(id, name, link);
-  })
-}, 60000);
+	console.log(colors.cyan('🟠 PCCOMPONENTES'));
+	if (pccomponentes_json.tracking)
+		if (pccomponentes_json.tracking.length == 0)
+			console.log("Missing index. Generate it with 'npm run indexs'");
+
+	pccomponentes_json.tracking.forEach(async ({ id, name, link }, index) => {
+		pccomponentes(id, name, link);
+	});
+}, 15000);
 
 setInterval(async () => {
-  console.log("\x1b[36mCHECKING VERSUS\x1b[0m");
-  for (const url of config.VERSUS_URL) {
-    versus(url);
-  }
-}, 60000)
+	console.log(colors.cyan('🟢 VERSUS GAMERS'));
+	for (const url of config.VERSUS_URL) {
+		versus(url);
+	}
+}, 15000);
 
 // COOLMOD
 setInterval(async () => {
-  console.log("\x1b[36mCHECKING COOLMOD\x1b[0m");
-  coolmod();
-}, 60000)
+	console.log(colors.cyan('🔵 COOLMOD'));
+	coolmod();
+}, 15000);
--- services/pccomponentes.js
+++ services/pccomponentes.js
@@ -1,24 +1,27 @@
 const axios = require('axios');
 const open = require('open');
+const colors = require('colors');
 const pccomponentes_json = require('../index/pccomponentes.json');
 
 var checked = [];
 
 module.exports = {
-  pccomponentes: async (id, name, link) => {
-    const linkAva = `${pccomponentes_json.urlAvailability}${id}`;
-    const {data} = await axios.get(linkAva)
-  
-    if(data.availability.status === 'outOfStock'){
-      if(checked.includes(name)){ 
-        checked = checked.filter(a=>a!=name);
-      }
-    }else {
-      console.log(`\x1b[32m[STOCK ${data.availability.realStock}] - ${name} - ${data.priceIntegerPart} € {${pccomponentes_json.url}${link}}\x1b[0m`);
-      if(!checked.includes(name)){ 
-        open(`${pccomponenpccomponentes_jsontes.url}${link}`);
-        checked.push(`${name}`)
-      };
-    }
-  }
-}
+	pccomponentes: async (id, name, link) => {
+		const linkAva = `${pccomponentes_json.urlAvailability}${id}`;
+		const { data } = await axios.get(linkAva);
+
+		if (data.availability.status === 'outOfStock') {
+			if (checked.includes(name)) checked = checked.filter((a) => a != name);
+		} else {
+			if (!checked.includes(name)) {
+				console.log(
+					colors.green(
+						`🟠 [STOCK ${data.availability.realStock}] - ${name} - ${data.priceIntegerPart} € {${pccomponentes_json.url}${link}}`
+					)
+				);
+				open(`${pccomponentes_json.url}${link}`);
+				checked.push(`${name}`);
+			}
+		}
+	},
+};
